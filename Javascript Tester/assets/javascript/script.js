8e3a801e12bc99d1f3fb1cceaa86115633a2d2cc
LilithN <LilithN@users.noreply.github.com>
2018-06-26 16:43:41 -0500

    loop version of fizzbuzz

--- Javascript Tester/assets/javascript/script.js
+++ Javascript Tester/assets/javascript/script.js
@@ -58,3 +58,24 @@ else if(randomNumber%5 === 0 && randomNumber != 0){
 else{
   console.log(randomNumber);
 }
+
+
+
+// Another version of fizzbuzz using a loop
+
+for (var i = 0; i <= 100; i++){
+  if(i%3 === 0){
+    if(i%5 === 0){
+      console.log("Fizzbuzz");
+    }
+    else{
+      console.log("fizz");
+    }
+  }
+  else if(i%5 === 0){
+    console.log("Buzz");
+  }
+  else{
+    console.log(i);
+  }
+}
